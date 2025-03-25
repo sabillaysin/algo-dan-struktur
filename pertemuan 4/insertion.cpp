53f3c1d313f97e3012a34213461e1fd61f1cc571
sabillaysin <sabillaysin@gmail.com>
2025-03-26 06:06:58 +0700

    procedure input

--- pertemuan 4/insertion.cpp
+++ pertemuan 4/insertion.cpp
@@ -4,4 +4,11 @@ using namespace std;
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat Variable inputan n
 
+void input()
+{ // Procedure Input
+    while (true)
+    {}
+
+}
+
 
