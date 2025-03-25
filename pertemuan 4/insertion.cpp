132979ed26c4487a473521f928ebd87ad86b42e8
sabillaysin <sabillaysin@gmail.com>
2025-03-26 06:23:51 +0700

    membuat tampilan

--- pertemuan 4/insertion.cpp
+++ pertemuan 4/insertion.cpp
@@ -15,7 +15,16 @@ void input()
         { // Membuat Kondisi n tidak lebih dari 20
             break;
         }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; // Menampilkan Pesan jika data lebih dari 20
+        }
     }
+    cout << endl; // Membuat jarak per baris program
+    cout << "============" << endl; // Membuat tampilan susunan data element array
+    cout << "Masukkan Element Array" << endl;
+    cout << "============" << endl;
+    
 
 }
 
