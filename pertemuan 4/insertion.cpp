95dd2356aeb4aeb622acaf35c9087973d063030a
sabillaysin <sabillaysin@gmail.com>
2025-03-26 06:31:04 +0700

    menyimpan nilai data

--- pertemuan 4/insertion.cpp
+++ pertemuan 4/insertion.cpp
@@ -26,7 +26,11 @@ void input()
     cout << "============" << endl;
 
     for (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
-    {}
+    { 
+        cout << "Data ke-" << (i + 1) << ": "; // Memasukkan atau menginputkan nilai data n
+        cin >> arr[i]; // Menyimpan nilai data n kedalam array arr
+        
+    }
 
 
 }
