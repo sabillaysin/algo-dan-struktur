cc0a976e0a1d7a0c7d7b99f14bdc91108ee110fb
sabillaysin <sabillaysin@gmail.com>
2025-03-26 06:10:15 +0700

    memanggil variable

--- pertemuan 4/insertion.cpp
+++ pertemuan 4/insertion.cpp
@@ -7,7 +7,10 @@ int n; // Membuat Variable inputan n
 void input()
 { // Procedure Input
     while (true)
-    {}
+    {
+        cout << "Masukkan Jumlah Data pada Array : "; // Membuat inputan jumlah element Array
+        cin >> n; // memanggil variable inputan n
+    }
 
 }
 
