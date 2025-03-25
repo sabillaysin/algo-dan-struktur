736a6b0c063f379ef7b05799951ed937b43d5070
sabillaysin <sabillaysin@gmail.com>
2025-03-25 23:40:32 +0700

    procedure insertion

--- Pertemuan04/insertion.cpp
+++ Pertemuan04/insertion.cpp
@@ -28,11 +28,13 @@ void input()
     for (int i = 0; i < n; i++) // Membuat perulangan for untuk menyimpan data pada array
     {
         cout << "Data ke-" << (i + 1) ":"; // Memasukkan atau menginputkan nilai data n
-        cin >> arr[i]; // Menyimpan nilai data n kedalam array arr
-        
+        cin >> arr[i]; // Menyimpan nilai data n kedalam array arr   
     }
+}
 
-
+void insertionSort()
+{ // Procedure Insertionsort
+    
 }
 
 
